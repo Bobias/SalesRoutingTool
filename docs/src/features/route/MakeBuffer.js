74e0dcf72bf1788a072220b25d9fa6bdb5374034
Bobias <34802607+Bobias@users.noreply.github.com>
2025-09-16 16:51:32 -0600

    Update MakeBuffer.js

--- docs/src/features/route/MakeBuffer.js
+++ docs/src/features/route/MakeBuffer.js
@@ -1,5 +1,5 @@
-export async function makeBufferFromFeature(feature, miles) {
-  const meters = miles * 1609.344;
-  const turf = await import('https://cdn.jsdelivr.net/npm/@turf/turf@6.5.0/turf.min.js');
-  return turf.buffer(feature, meters, { units: 'meters' });
+export async function MakeBuffer(Feature, Miles) {
+  const Meters = Miles * 1609.344;
+  const Turf = await import('https://cdn.jsdelivr.net/npm/@turf/turf@6.5.0/turf.min.js');
+  return Turf.buffer(Feature, Meters, { units: 'meters' });
 }
